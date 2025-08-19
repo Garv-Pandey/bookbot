28602859ef879445cd53c4014b6205cae4b1a553
Garv-Pandey <pandey.garv25@gmail.com>
2025-08-19 08:38:16 +0000

    add word count for book content

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
+def word_count(content):
+    content_list = content.split()
+    return len(content_list)
+
 def main():
     print ("main")
     contents = get_book_text("./books/frankenstein.txt") 
-    print(contents)
+    num_words = word_count(contents)
+    print(f'{num_words} words found in the document')
 
     return 0
 
