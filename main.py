050edd29328b65efb90eb01fa372460ae68998c3
Garv-Pandey <pandey.garv25@gmail.com>
2025-08-19 09:16:55 +0000

    build bookbot with hardcoded argument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import word_count
+from stats import char_count
+from stats import char_sort
 
 def get_book_text(file_path):
     file_contents = None
@@ -7,10 +9,24 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    print ("main")
-    contents = get_book_text("./books/frankenstein.txt") 
+    print("============ BOOKBOT ============")
+
+    book_address = "books/frankenstein.txt"
+    print (f"Analyzing book found at {book_address}")
+
+
+    contents = get_book_text(f"./{book_address}") 
+
+    print("----------- Word Count ----------")
     num_words = word_count(contents)
-    print(f'{num_words} words found in the document')
+    print(f'Found {num_words} total words')
+
+
+    print("----------- Character Count----------")
+    data = char_sort(char_count(contents))
+
+    for d in data:
+        print(f'{d["char"]}: {d["num"]}')
 
     return 0
 
--- stats.py
+++ stats.py
@@ -1,3 +1,33 @@
 def word_count(content):
     content_list = content.split()
     return len(content_list)
+
+def char_count(content):
+    char_count = dict()
+
+    for char in content:
+        char = char.lower()
+        if char not in char_count:
+            char_count[char] = 1
+            continue
+
+        char_count[char] += 1
+
+    return char_count
+
+def char_sort(char_dict):
+    sorted_char_list = sorted(char_dict.items(), key=lambda item: item[1], reverse=True)
+    # Example result: [('z', 25), ('a', 20), ('x', 15), ...]
+
+    return_value = []
+
+    for char_obj in sorted_char_list:
+        char = char_obj[0]
+        count = char_obj[1]
+
+        if not char.isalpha():
+            continue
+
+        return_value.append({"char": char, "num": count})
+
+    return return_value
